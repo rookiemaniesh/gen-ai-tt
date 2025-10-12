cb0c115b791f3f041e8fb3b006b62c50d5838c87
manish <imh10015.23@bitmesra.ac.in>
2025-10-12 10:47:21 +0530

    chat-model

--- 2.Chatmodel/chatmodelgemini.py
+++ 2.Chatmodel/chatmodelgemini.py
@@ -1,1 +1,7 @@
-from langchain_google_genai import 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+load_dotenv()
+
+model=ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+result=model.invoke('who are you?')
+print(result.content)
