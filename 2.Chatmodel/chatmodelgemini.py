899d5d91404d0d5b5f0f75b0b0766c16f629da75
manish <imh10015.23@bitmesra.ac.in>
2025-10-12 12:13:17 +0530

    hugging face api model

--- 2.Chatmodel/chatmodelgemini.py
+++ 2.Chatmodel/chatmodelgemini.py
@@ -2,6 +2,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 load_dotenv()
 
-model=ChatGoogleGenerativeAI(model='gemini-2.5-flash')
-result=model.invoke('who are you?')
+model=ChatGoogleGenerativeAI(model='gemini-2.5-flash',temperature=1, max_completion_tokens=10)
+result=model.invoke('Hows life?')
 print(result.content)
