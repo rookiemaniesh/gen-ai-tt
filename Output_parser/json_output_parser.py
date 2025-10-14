165e8a05d7ac7ebcdcac1cb696850387f1ae8bca
manish <imh10015.23@bitmesra.ac.in>
2025-10-14 18:33:13 +0530

    output parser json

--- Output_parser/json_output_parser.py
+++ Output_parser/json_output_parser.py
@@ -21,4 +21,22 @@ template=PromptTemplate(
 
 )
 prompt=template.format()
-print(prompt)
+# result=model.invoke(prompt)
+# # print(result)
+# final_result=parser.parse(result.content)
+# print(final_result)
+
+chain= template | model | parser
+result=chain.invoke({}) #input variable khali hai iss liye 
+print(result)
+
+# dekha schema validation nhi hai isme
+# Invalid json output: Please provide me with the name of the fictional character you'd like the information for! 😉
+
+# Once I know the character's name, I can give you their age and city.
+
+# For example, you could say:
+
+# "Give the name, age, and city of the fictional character **Atticus Finch**."
+
+# Then, I'll give you the answer in JSON format. 😊
